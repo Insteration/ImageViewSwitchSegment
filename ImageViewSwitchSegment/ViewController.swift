6781624f5f251a7d41568fd16c3d1b6419c44580
Artem Karmaz <inste@icloud.com>
2018-12-19 20:57:14 +0200

    added storyboard

--- ImageViewSwitchSegment/ViewController.swift
+++ ImageViewSwitchSegment/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var iPhoneImage: UIImageView!
+    @IBOutlet weak var iPadImage: UIImageView!
+    @IBAction func actionSwitch(_ sender: UISwitch) {
+        textLabel.text = sender.isOn ? "True": "False"
+        iPhoneImage.contentMode = sender.isOn ? .scaleAspectFit : .scaleToFill
+    }
+    @IBAction func changeColorSelector(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            iPadImage.backgroundColor = .white
+        case 1:
+            iPadImage.backgroundColor = .black
+        case 2:
+            iPadImage.backgroundColor = .green
+        case 3:
+            iPadImage.backgroundColor = .blue
+        case 4:
+            iPadImage.backgroundColor = .orange
+        default:
+            ()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
